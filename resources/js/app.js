876ab61e580442d2c31c129c973932980e47789e
developer-tamim <tawsiftamim24@gmail.com>
2025-04-14 18:01:22 +0600

    dashboard stting up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,26 @@
 import '../css/app.css';
 import './bootstrap';
 
+// css
+import './assets/images/favicon.ico';
+import './assets/css/bootstrap.min.css';
+import './assets/css/icons.min.css';
+import './assets/css/app.min.css';
+import './assets/js/plugin.js';
+// css
+
+// js
+import './assets/libs/jquery/jquery.min.js';
+import './assets/libs/bootstrap/js/bootstrap.bundle.min.js';
+import './assets/libs/metismenu/metisMenu.min.js';
+import './assets/libs/simplebar/simplebar.min.js';
+import './assets/libs/node-waves/waves.min.js';
+import './assets/libs/apexcharts/apexcharts.min.js';
+import './assets/js/pages/dashboard.init.js';
+import './assets/js/app.js';
+// js
+
+
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
