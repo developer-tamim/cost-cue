e66a03905a689862e0f99c82e01314221500b6e0
developer-tamim <tawsiftamim24@gmail.com>
2025-05-29 11:39:30 +0600

    main project up

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,48 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, SoftDeletes, Notifiable, InteractsWithMedia, HasRoles;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'phone',
+        'dob',
+        'gender',
+        'status',
+        'first_name',
+        'last_name',
+        'postal_code',
+        'country_id',
+        'state_id',
+        'location',
+        'about_me',
+        'bio',
+        'skills',
+        'country_code',
+        'created_by_id',
+        'system_reserve',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -41,8 +58,48 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+           'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'phone' => 'integer',
+            'status' => 'integer',
+            'created_by_id' => 'integer'
         ];
+    }
+
+    protected $appends = [
+        'role',
+    ];
+
+    protected $with = [
+        'media'
+    ];
+
+    public static function booted()
+    {
+        parent::boot();
+        static::saving(function ($model) {
+            $model->created_by_id = \App\Helpers\Helpers::isUserLogin() ? \App\Helpers\Helpers::getCurrentUserId() : $model->id;
+        });
+    }
+
+    /**
+     * Get the user's role.
+     */
+    public function getRoleAttribute()
+    {
+        return $this->roles->first()?->makeHidden(['created_at', 'updated_at', 'pivot']);
+    }
+
+    /**
+     * Get the user's all permissions.
+     */
+    public function getPermissionAttribute()
+    {
+        return $this->getAllPermissions();
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class,'country_id');
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'public/assets/scss/style.scss',
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
         }),
     ],
 });
